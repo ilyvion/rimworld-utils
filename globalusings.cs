e677678a7ae4045619bd372bfab3db22275f4ac0
Alexander Krivács Schrøder <alexschrod@gmail.com>
2025-08-28 05:06:49 +0200

    feat: add support for laboratory feature toggles in Common.props and update global usings

--- globalusings.cs
+++ globalusings.cs
@@ -1,14 +1,17 @@
 #pragma warning disable IDE0005 // Using directive is unnecessary.
+global using System.Globalization;
+global using System.Reflection;
+global using System.Runtime.CompilerServices;
+
 global using HarmonyLib;
+
+global using UnityEngine;
 
 global using RimWorld;
 global using Verse;
-global using UnityEngine;
 
-global using System;
-global using System.Collections;
-global using System.Collections.Generic;
-global using System.Linq;
-global using System.Reflection;
-global using System.Runtime.CompilerServices;
+global using ilyvion.Laboratory;
+global using ilyvion.Laboratory.Coroutines;
+global using Coroutine = System.Collections.Generic.IEnumerable<
+    ilyvion.Laboratory.Coroutines.IResumeCondition>;
 #pragma warning restore IDE0005 // Using directive is unnecessary.
