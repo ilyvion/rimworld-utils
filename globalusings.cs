d2d1407d5b91abdc8f242fb7e030892862f445f5
Alexander Krivács Schrøder <alexschrod@gmail.com>
2025-09-01 09:58:11 +0200

    fix: enable conditional usage of Laboratory features based on UseLaboratory property

--- globalusings.cs
+++ globalusings.cs
@@ -3,10 +3,12 @@ global using System.Globalization;
 global using System.Reflection;
 global using System.Runtime.CompilerServices;
 global using HarmonyLib;
-global using ilyvion.Laboratory;
-global using ilyvion.Laboratory.Coroutines;
 global using RimWorld;
 global using UnityEngine;
 global using Verse;
+#if USE_LABORATORY
+global using ilyvion.Laboratory;
+global using ilyvion.Laboratory.Coroutines;
 global using Coroutine = System.Collections.Generic.IEnumerable<ilyvion.Laboratory.Coroutines.IResumeCondition>;
+#endif
 #pragma warning restore IDE0005 // Using directive is unnecessary.
